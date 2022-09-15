45cbbf72be535f72a793da4bd9876ccb013e41a6
Danilo Costa <dancostasf@gmail.com>
2022-09-15 13:53:13 -0300

    Implementando tasks

--- app/src/main/java/br/com/android/exemplopix/InfoFragment.kt
+++ app/src/main/java/br/com/android/exemplopix/InfoFragment.kt
@@ -1,17 +1,23 @@
 package br.com.android.exemplopix
 
+import android.app.AlertDialog
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.navArgs
 import br.com.android.exemplopix.commons.navBack
+import br.com.android.exemplopix.commons.navTo
 import br.com.android.exemplopix.databinding.FragmentInfoBinding
 
 class InfoFragment : Fragment() {
 
     private var _binding: FragmentInfoBinding? = null
     private val binding get() = _binding!!
+
+    private val args: InfoFragmentArgs by navArgs()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -24,21 +30,36 @@ class InfoFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        val list = args.nomes?.toMutableList()
+        val lista = list?.joinToString(separator = ", ") {
+            it
+        }
 
+        Log.i("teste", lista.toString())
         binding.apply {
-            setupToolbar()
+            setupNavigationListener()
         }
     }
 
-    private fun FragmentInfoBinding.setupToolbar() {
-        toolbar.setOnMenuItemClickListener { menuItem ->
-            when (menuItem.itemId) {
-                R.id.back_button_info -> {
-                    navBack()
-                    true
-                }
-                else -> false
-            }
+    private fun FragmentInfoBinding.setupNavigationListener() {
+        toolbar.setNavigationOnClickListener {
+            setupBackButton()
         }
+    }
+
+    private fun setupBackButton() {
+        binding.toolbar.isEnabled = false
+        val builder = AlertDialog.Builder(requireContext())
+        builder.setTitle("Dúvida?")
+        builder.setMessage("Tem alguma dúvida?")
+        builder.setPositiveButton("Sim") { _, _ ->
+            navBack()
+        }
+        builder.setNegativeButton("Não") { _, _ ->
+            binding.toolbar.isEnabled = true
+        }
+        val alertDialog: AlertDialog = builder.create()
+        alertDialog.setCancelable(true)
+        alertDialog.show()
     }
 }
--- app/src/main/java/br/com/android/exemplopix/PixFragment.kt
+++ app/src/main/java/br/com/android/exemplopix/PixFragment.kt
@@ -2,39 +2,34 @@ package br.com.android.exemplopix
 
 import android.app.AlertDialog
 import android.os.Bundle
-import android.text.Editable
-import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.EditText
 import androidx.fragment.app.Fragment
+import br.com.android.exemplopix.commons.MaskMoney
+import br.com.android.exemplopix.commons.MaskMoney.Companion.numero
 import br.com.android.exemplopix.commons.navTo
 import br.com.android.exemplopix.databinding.FragmentPixBinding
 import com.google.android.material.snackbar.Snackbar
-import java.lang.ref.WeakReference
-import java.math.BigDecimal
-import java.text.NumberFormat
 import java.util.*
 
 // Se o valor estiver vazio, desabilitar o botão Próximo, portanto tu pode remover o teste de o valor estar vazio no show toast.( )
-// Replicar botão do navigation E quando clicar em voltar, perguntar em um novo dialog se usuário quer voltar mesmo. Se clicar em não, continua na tela de dúvidas. Se clicar em sim, fechar a tela atual.
-// Colocar MaskMoney em outro package common
-// O valor/TOque para visualizar seja alterado de acordo com o container
-// Controlar quando clicar no dialog para não permitir abrir duas vezes o dialog
-// Colocar click fora para fechar todos os dialogs.
+// Replicar botão do navigation E quando clicar em voltar, perguntar em um novo dialog se usuário quer voltar mesmo. Se clicar em não, continua na tela de dúvidas. Se clicar em sim, fechar a tela atual.(X)
+// Colocar MaskMoney em outro package common(Falta desabilitar o button)
+// O valor/TOque para visualizar seja alterado de acordo com o container(X)
+// Controlar quando clicar no dialog para não permitir abrir duas vezes o dialog(X)
+// Colocar click fora para fechar todos os dialogs.(X)
 // Criar um parametro para passar para o fragment de dúvidas. Parametro: List<String>?. Quando tu chegar na tela de dúvidas, formatar as Strings em uma String só no seguinte formato:
 // Aleluia, Aiaiai, Danilo é brabo. Se passar apenas uma, tem que ter apenas o ponto. Ex: Danilo. Se passar uma lista sem items ou o parametro nulo, exibir mensagem default: Nenhum parametro para listar.
-// Na primeira tela, os campos não podem se sobrepor.
-// Na segunda tela, a frase "Voce chegou..." precisa estar centralizada.
-// Colocar um valor máximo de 99.999,99
-//
+// Na primeira tela, os campos não podem se sobrepor.(X)
+// Na segunda tela, a frase "Voce chegou..." precisa estar centralizada.(X)
+// Colocar um valor máximo de 99.999,99(X)
+
 class PixFragment : Fragment() {
 
     private var _binding: FragmentPixBinding? = null
     private val binding get() = _binding!!
 
-    private var numero: String? = "R\$ 0,00"
     // Legal no onCreateView() é ter apenas uma função, que é inflar o layout (O que na verdade nem vamos mais fazer pq vai estar no BaseFragment)
 
     // Agora para adicionar comportamentos, o legal é utilizar o onViewCreated()
@@ -54,8 +49,13 @@ class PixFragment : Fragment() {
         val mlocal = Locale("pt", "BR")
         binding.editText.addTextChangedListener(MaskMoney(binding.editText, mlocal))
 
+        val nomes = arrayOf(
+            "teste 1",
+            "teste 2"
+        )
+
         binding.apply {
-            setupToolbar()
+            setupToolbar(nomes)
             setupNavigationListener()
             setupNextButton()
             setupToqueButton()
@@ -68,37 +68,41 @@ class PixFragment : Fragment() {
         }
     }
 
-    private fun FragmentPixBinding.setupToolbar() {
+    private fun FragmentPixBinding.setupToolbar(list: Array<String>?) {
         toolbar.setOnMenuItemClickListener { menuItem ->
             if (menuItem.itemId == R.id.info_button) {
-                setupInfoButton()
+                menuItem.isEnabled = false
+                setupInfoButton(list)
             }
+            menuItem.isEnabled = true
             true
         }
     }
 
-    private fun setupInfoButton() {
+    private fun setupInfoButton(list: Array<String>?) {
         val builder = AlertDialog.Builder(requireContext())
         builder.setTitle("Dúvida?")
         builder.setMessage("Tem alguma dúvida?")
         builder.setPositiveButton("Sim") { _, _ ->
-            navTo(PixFragmentDirections.actionPixFragmentToInfoFragment())
+            navTo(PixFragmentDirections.actionPixFragmentToInfoFragment(
+                list
+            ))
         }
         builder.setNegativeButton("Não") { _, _ -> }
         val alertDialog: AlertDialog = builder.create()
-        alertDialog.setCancelable(false)
+        alertDialog.setCancelable(true)
         alertDialog.show()
     }
 
     private fun FragmentPixBinding.setupToqueButton() {
-        txtvToqueVizualizar.setOnClickListener {
-            binding.txtvToqueVizualizar.visibility = View.GONE
-            binding.txtValor.visibility = View.VISIBLE
-        }
-
-        txtValor.setOnClickListener {
-            binding.txtvToqueVizualizar.visibility = View.VISIBLE
-            binding.txtValor.visibility = View.GONE
+        clickMoneyShow.setOnClickListener {
+            if (binding.txtvToqueVizualizar.visibility == View.GONE) {
+                binding.txtvToqueVizualizar.visibility = View.VISIBLE
+                binding.txtValor.visibility = View.GONE
+            } else {
+                binding.txtvToqueVizualizar.visibility = View.GONE
+                binding.txtValor.visibility = View.VISIBLE
+            }
         }
     }
 
@@ -111,46 +115,8 @@ class PixFragment : Fragment() {
             )
             snackbar.show()
         }
-
-        nextButton.isEnabled = false
-    }
-
-    inner class MaskMoney(editText: EditText?, locale: Locale?) : TextWatcher {
-        private val editTextReferenceWeak: WeakReference<EditText>
-        private val locale: Locale
-
-        init {
-            editTextReferenceWeak = WeakReference<EditText>(editText)
-            this.locale = locale ?: Locale.getDefault()
-        }
-
-        override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {}
-        override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {}
-        override fun afterTextChanged(editable: Editable) {
-
-            val editText: EditText = editTextReferenceWeak.get() ?: return
-            editText.removeTextChangedListener(this)
-            val parsed: BigDecimal = parseToBigDecimal(editable.toString(), locale)
-            binding.nextButton.isEnabled = parsed > BigDecimal(0)
-            val formatted: String = NumberFormat.getCurrencyInstance(locale).format(parsed)
-            editText.setText(formatted)
-            numero = formatted
-
-            editText.setSelection(formatted.length)
-            editText.addTextChangedListener(this)
-        }
-
-        private fun parseToBigDecimal(value: String, locale: Locale): BigDecimal {
-            val replaceable = java.lang.String.format(
-                "[%s,.\\s]",
-                NumberFormat.getCurrencyInstance(locale).currency?.symbol
-            )
-            val cleanString = value.replace(replaceable.toRegex(), "")
-            return BigDecimal(cleanString).setScale(
-                2, BigDecimal.ROUND_FLOOR
-            ).divide(
-                BigDecimal(100), BigDecimal.ROUND_FLOOR
-            )
-        }
     }
 }
+
+
+

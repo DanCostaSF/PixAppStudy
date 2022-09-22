0cf8b6bd0bf33763769da36c891f17c3c2e155aa
Danilo Costa <dancostasf@gmail.com>
2022-09-22 17:19:26 -0300

    Implementando tasks - 8

--- app/src/main/java/br/com/android/exemplopix/TransferViewModel.kt
+++ app/src/main/java/br/com/android/exemplopix/TransferViewModel.kt
@@ -27,7 +27,7 @@ class TransferViewModel : ViewModel() {
                             it.substring(6, 9) + "-" +
                             it.substring(9, 11)
                     snackBarString.value = "TIPO: CPF - VALOR: $cpf"
-                    true
+                    isCPF(it)
                 } catch (e: NumberFormatException) {
                     snackBarString.value = "TIPO: EMAIL - VALOR:$it"
                     emailVerify(it)
@@ -53,6 +53,28 @@ class TransferViewModel : ViewModel() {
                 emailVerify(it)
             }
         }
+    }
+
+    private fun isCPF(document: String): Boolean {
+        if (document.isEmpty()) return false
+
+        val numbers = document.filter { it.isDigit() }.map {
+            it.toString().toInt()
+        }
+
+        if (numbers.size != 11) return false
+
+        if (numbers.all { it == numbers[0] }) return false
+
+        val dv1 = ((0..8).sumOf { (it + 1) * numbers[it] }).rem(11).let {
+            if (it >= 10) 0 else it
+        }
+
+        val dv2 = ((0..8).sumOf { it * numbers[it] }.let { (it + (dv1 * 9)).rem(11) }).let {
+            if (it >= 10) 0 else it
+        }
+
+        return numbers[9] == dv1 && numbers[10] == dv2
     }
 
     private fun emailVerify(email: String) : Boolean {

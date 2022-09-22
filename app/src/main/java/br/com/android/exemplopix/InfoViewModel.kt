0e8513e57412ee484e28a1f7093e9ff7bb5dccb4
Danilo Costa <dancostasf@gmail.com>
2022-09-22 09:36:19 -0300

    Implementando tasks - 7

--- app/src/main/java/br/com/android/exemplopix/InfoViewModel.kt
+++ app/src/main/java/br/com/android/exemplopix/InfoViewModel.kt
@@ -2,17 +2,26 @@ package br.com.android.exemplopix
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.map
 import androidx.lifecycle.viewModelScope
 
 class InfoViewModel : ViewModel() {
 
     val textViewInfo = MutableLiveData("")
 
-   val text = MutableLiveData(if (textViewInfo.value.isNullOrEmpty()) {
-       ""
-   } else {
-       "Teste ${textViewInfo.value.toString()}"
-   })
+    val onNavigateBack = MutableLiveData(false)
+
+    fun onNavigationClick() {
+        onNavigateBack.value = true
+    }
+
+    val text = textViewInfo.map {
+        if (it.isEmpty()) {
+            ""
+        } else {
+            "Texto complementar - $it"
+        }
+    }
 
 }
 
